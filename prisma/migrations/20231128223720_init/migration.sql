1992c02dbc421b6f3c2f17422fe18f00d7f1e289
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-18 18:21:26 +0100

    chore: delete old migrations

--- prisma/migrations/20231128223720_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "ReplicacheClient" ADD COLUMN     "lastModifiedVersion" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "lastMutationId" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20231129231233_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `archived` to the `Habit` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Habit" ADD COLUMN     "archived" BOOLEAN NOT NULL;

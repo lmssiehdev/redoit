db46d411091ce40920ae5849c9cadea9a7250ecc
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-12-23 23:42:31 +0100

    feat: add streak to habit detail page

--- src/app/(replicache)/habit/[id]/page.tsx
+++ src/app/(replicache)/habit/[id]/page.tsx
@@ -3,11 +3,17 @@
 import { AppBackButton } from "@/app/(replicache)/create/page";
 import { MonthlyCalendar } from "@/components/calendar/Monthly";
 import ChartWrapper from "@/components/chart";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { HabitProvider, useHabit } from "@/context/HabitProvider";
 import { useReplicacheFromContext } from "@/context/ReplicacheProvider";
+import { cn } from "@/utils/misc";
+import streakRanges from "@/utils/streakRanges";
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
+import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
+import { useMemo } from "react";
+import { convertHexToRGBA } from "@/utils/misc";
+import Link from "next/link";
 
 export default function HabitPage({ params }: { params: { id: string } }) {
   return (
@@ -33,6 +39,8 @@ function Content() {
     router.push("/web");
   }
 
+  // return ;
+
   return (
     <div className="max-w-screen-sm w-full mx-auto">
       <div className="max-w-fit w-full mx-auto">
@@ -47,7 +55,15 @@ function Content() {
               {habitData.name}
             </div>
             <div className="flex gap-2 items-center">
-              <Pencil1Icon className="h-4 w-4" />
+              <Link
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "icon",
+                })}
+                href={`/edit/${habitId.replace("habit/", "")}`}
+              >
+                <Pencil1Icon className="h-4 w-4" />
+              </Link>
               <Button variant="ghost" size="icon" onClick={handleDeleteHabit}>
                 <TrashIcon className="h-4 w-4" />
               </Button>
@@ -56,6 +72,7 @@ function Content() {
           <div className="my-5">
             <MonthlyCalendar />
           </div>
+          <Streaks />
         </div>
         {/* <div>
           <h2 className="text-center font-bold text-lg tracking-wide">
@@ -63,6 +80,72 @@ function Content() {
           </h2>
           <ChartWrapper color={habitData.color} />
         </div> */}
+      </div>
+    </div>
+  );
+}
+
+function Streaks() {
+  const { completedDates } = useHabit();
+  const {
+    habitData: { color },
+  } = useHabit();
+  const ranges = useMemo(() => {
+    function getTop5RangesPercentages(
+      ranges: ReturnType<typeof streakRanges>
+    ): number[] {
+      const totalSum = ranges.reduce((sum, range) => sum + range.duration, 0);
+      const percentages = ranges.map(
+        (range) => (range.duration / totalSum) * 100
+      );
+      return percentages;
+    }
+
+    const ranges = streakRanges(
+      Object.keys(completedDates) as unknown as Date[]
+    );
+
+    const sortedRanges = ranges.sort((a, b) => b.duration - a.duration);
+    const top5Ranges = sortedRanges.slice(0, 4);
+    const top5RangesPercentages = getTop5RangesPercentages(top5Ranges);
+
+    const updatedRage = top5Ranges.map((item, index) => {
+      return {
+        ...item,
+        percentage: top5RangesPercentages[index],
+      };
+    });
+
+    return updatedRage;
+  }, [completedDates]);
+
+  return (
+    <div>
+      <h2 className="text-xl text-center mb-5">Streaks</h2>
+      <div className="flex flex-col items-center justify-center gap-2 max-w-[400px] ">
+        {ranges.map((item, index) => (
+          <div key={index} className="flex-1 w-full flex justify-center gap-2 ">
+            <span className="whitespace-nowrap text-xs flex items-center">
+              {dayjs(new Date(item.start)).format("MMM DD")}
+            </span>
+            <div
+              className={cn(
+                "rounded-[10px_0/100px_20px] yellow text-center justify-center px-1 min-w-fit py-[0.1rem] transition-width duration-1000 ease-in-out"
+              )}
+              style={{
+                width: `${item.percentage}%`,
+                ...convertHexToRGBA(color),
+              }}
+            >
+              <span className="mx-1 block">{item.duration}</span>
+            </div>
+            <span className="whitespace-nowrap text-xs flex items-center">
+              {dayjs(new Date(item.end ? item.end : item.start)).format(
+                "MMM DD"
+              )}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -30,3 +30,29 @@ export const generateId = (n = 6) => {
 
   return nanoid();
 };
+
+export function convertHexToRGBA(hexCode = "", opacity = 1) {
+  let hex = hexCode.replace("#", "");
+
+  if (hex.length === 3) {
+    hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
+  }
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  /* Backward compatibility for whole number based opacity values. */
+  if (opacity > 1 && opacity <= 100) {
+    opacity = opacity / 100;
+  }
+  // `rgba(${r},${g},${b},${opacity})`
+  const rgbString = `${r},${g},${b}`;
+
+  const highlight = {
+    background: `
+    linear-gradient(to right, rgba(${rgbString}, 0.3) 0%, rgba(${rgbString}, 0.4) 60%, rgba(${rgbString}, 0.4) 60%, rgba(${rgbString}, 0.6) 85%, rgba(${rgbString}, 0.8) 100%)
+    `,
+  };
+  return highlight;
+}

9e26f26b3aed1b8eab2ef8adcffe086b18f9611e
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-10-26 18:46:41 +0100

    use nextjs rewrite for posthog

--- apps/offline-nextjs/next.config.ts
+++ apps/offline-nextjs/next.config.ts
@@ -8,8 +8,21 @@ const nextConfig: NextConfig = {
 				source: "/feedback",
 				destination: "https://tally.so/r/wvMWaA",
 			},
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/ingest/decide",
+				destination: "https://us.i.posthog.com/decide",
+			},
 		];
 	},
+	skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;

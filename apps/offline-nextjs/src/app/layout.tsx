ed2a0d469ae33a438b8534b3a1827d6e5b985d11
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-10-27 14:36:24 +0100

    chore: lint

--- apps/offline-nextjs/src/app/layout.tsx
+++ apps/offline-nextjs/src/app/layout.tsx
@@ -7,86 +7,86 @@ import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "redoit!",
-  description: "Your radically easy-to-use habit tracker",
+	title: "redoit!",
+	description: "Your radically easy-to-use habit tracker",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <PostHogProviderWrapper>
-        <body
-          className={cn(
-            [
-              neucha.variable,
-              pacifico.variable,
-              mathlete.variable,
-              excalifont.variable,
-            ],
-            "font-display px-2 min-h-screen flex flex-col"
-          )}
-        >
-          <nav className="py-2 flex text-center items-center justify-between gap-2 text-lg">
-            <Link
-              href="/"
-              // activeProps={{
-              //   className: "font-bold",
-              // }}
-              // activeOptions={{ exact: true }}
-            >
-              Redoit<span className="pl-1 text-xs text-gray-400">[beta]</span>
-            </Link>
+	return (
+		<html lang="en">
+			<PostHogProviderWrapper>
+				<body
+					className={cn(
+						[
+							neucha.variable,
+							pacifico.variable,
+							mathlete.variable,
+							excalifont.variable,
+						],
+						"font-display px-2 min-h-screen flex flex-col",
+					)}
+				>
+					<nav className="py-2 flex text-center items-center justify-between gap-2 text-lg">
+						<Link
+							href="/"
+							// activeProps={{
+							//   className: "font-bold",
+							// }}
+							// activeOptions={{ exact: true }}
+						>
+							Redoit<span className="pl-1 text-xs text-gray-400">[beta]</span>
+						</Link>
 
-            <div className="flex items-center gap-2">
-              <Button asChild variant="link" className="px-1">
-                <Link
-                  href="/archive"
-                  // activeProps={{
-                  //   className: "underline",
-                  // }}
-                >
-                  archived
-                </Link>
-              </Button>
-              <Button asChild variant="link" className="px-1">
-                <Link
-                  href="/settings"
-                  // activeProps={{
-                  //   className: "underline",
-                  // }}
-                >
-                  settings
-                </Link>
-              </Button>
-              <Link
-                target="_blank"
-                href="https://github.com/lmssiehdev/redoit-monorepo"
-                className={cn(
-                  buttonVariants({ size: "sm" }),
-                  "flex items-center gap-1"
-                )}
-              >
-                ⭐ Star us on github
-              </Link>
-            </div>
-          </nav>
-          <div className="flex-1">{children}</div>
-          <footer className="text-sm my-2 hidden sm:block ml-auto">
-            🌱 built with care, one day at a time{" "}
-            <span className="font-pacifico">{" • "}</span>
-            <Link
-              href="/feedback"
-              className="text-[#65a30d] hover:opacity-70 underline underline-offset-2 font-bold"
-            >
-              send feedback ↗
-            </Link>
-          </footer>
-        </body>
-      </PostHogProviderWrapper>
-    </html>
-  );
+						<div className="flex items-center gap-2">
+							<Button asChild variant="link" className="px-1">
+								<Link
+									href="/archive"
+									// activeProps={{
+									//   className: "underline",
+									// }}
+								>
+									archived
+								</Link>
+							</Button>
+							<Button asChild variant="link" className="px-1">
+								<Link
+									href="/settings"
+									// activeProps={{
+									//   className: "underline",
+									// }}
+								>
+									settings
+								</Link>
+							</Button>
+							<Link
+								target="_blank"
+								href="https://github.com/lmssiehdev/redoit-monorepo"
+								className={cn(
+									buttonVariants({ size: "sm" }),
+									"flex items-center gap-1",
+								)}
+							>
+								⭐ Star us on github
+							</Link>
+						</div>
+					</nav>
+					<div className="flex-1">{children}</div>
+					<footer className="text-sm my-2 hidden sm:block ml-auto">
+						🌱 built with care, one day at a time{" "}
+						<span className="font-pacifico">{" • "}</span>
+						<Link
+							href="/feedback"
+							className="text-[#65a30d] hover:opacity-70 underline underline-offset-2 font-bold"
+						>
+							send feedback ↗
+						</Link>
+					</footer>
+				</body>
+			</PostHogProviderWrapper>
+		</html>
+	);
 }
--- apps/offline-nextjs/src/components/calendar/monthly/charts.tsx
+++ apps/offline-nextjs/src/components/calendar/monthly/charts.tsx
@@ -4,9 +4,9 @@ import { useHabit } from "@/app/habit/[id]/_provider";
 import { normalizeColor } from "@/components/calendar/monthly/overview";
 import type { ChartConfig } from "@/components/ui/chart";
 import {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
 } from "@/components/ui/chart";
 import { DAYS } from "@/constants";
 import { type HabitData, Status } from "@/types";
@@ -14,79 +14,79 @@ import dayjs from "dayjs";
 import { Bar, BarChart, XAxis } from "recharts";
 
 const chartData = (dates: HabitData["dates"]) => {
-  const rate = Array.from({ length: 7 }, () => ({
-    [Status.Skipped]: 0,
-    [Status.Completed]: 0,
-  }));
+	const rate = Array.from({ length: 7 }, () => ({
+		[Status.Skipped]: 0,
+		[Status.Completed]: 0,
+	}));
 
-  Object.entries(dates).map(
-    ([date, status]) => rate[dayjs(date).day()][status]++
-  );
+	Object.entries(dates).map(
+		([date, status]) => rate[dayjs(date).day()][status]++,
+	);
 
-  return DAYS.map((date, index) => ({
-    date,
-    completed: rate[index][Status.Completed],
-    skipped: rate[index][Status.Skipped],
-  }));
+	return DAYS.map((date, index) => ({
+		date,
+		completed: rate[index][Status.Completed],
+		skipped: rate[index][Status.Skipped],
+	}));
 };
 
 export const description = "A stacked bar chart with a legend";
 
 const chartConfig = (color: string) => {
-  const { chartCompletedColor, chartSkippedColor } = normalizeColor(color);
+	const { chartCompletedColor, chartSkippedColor } = normalizeColor(color);
 
-  return {
-    activities: {
-      label: "Activities",
-    },
-    completed: {
-      label: "Completed",
-      color: chartCompletedColor,
-    },
-    skipped: {
-      label: "Skipped",
-      color: chartSkippedColor,
-    },
-  } satisfies ChartConfig;
+	return {
+		activities: {
+			label: "Activities",
+		},
+		completed: {
+			label: "Completed",
+			color: chartCompletedColor,
+		},
+		skipped: {
+			label: "Skipped",
+			color: chartSkippedColor,
+		},
+	} satisfies ChartConfig;
 };
 
 export function WeekChart() {
-  const {
-    habitData: { color, dates },
-  } = useHabit();
+	const {
+		habitData: { color, dates },
+	} = useHabit();
 
-  return (
-    <>
-      <ChartContainer config={chartConfig(color)}>
-        <BarChart accessibilityLayer data={chartData(dates)}>
-          <XAxis
-            dataKey="date"
-            tickLine={false}
-            tickMargin={10}
-            axisLine={false}
-            tickFormatter={(value: string) => value.substring(0, 3)}
-          />
-          <Bar
-            dataKey="completed"
-            stackId="a"
-            fill="var(--color-completed)"
-            radius={[0, 0, 4, 4]}
-          />
-          <Bar
-            dataKey="skipped"
-            stackId="a"
-            fill="var(--color-skipped)"
-            radius={[4, 4, 0, 0]}
-          />
-          <ChartTooltip
-            content={
-              <ChartTooltipContent labelKey="activities" indicator="line" />
-            }
-            cursor={false}
-            // defaultIndex={1}
-          />
-        </BarChart>
-      </ChartContainer>
-    </>
-  );
+	return (
+		<>
+			<ChartContainer config={chartConfig(color)}>
+				<BarChart accessibilityLayer data={chartData(dates)}>
+					<XAxis
+						dataKey="date"
+						tickLine={false}
+						tickMargin={10}
+						axisLine={false}
+						tickFormatter={(value: string) => value.substring(0, 3)}
+					/>
+					<Bar
+						dataKey="completed"
+						stackId="a"
+						fill="var(--color-completed)"
+						radius={[0, 0, 4, 4]}
+					/>
+					<Bar
+						dataKey="skipped"
+						stackId="a"
+						fill="var(--color-skipped)"
+						radius={[4, 4, 0, 0]}
+					/>
+					<ChartTooltip
+						content={
+							<ChartTooltipContent labelKey="activities" indicator="line" />
+						}
+						cursor={false}
+						// defaultIndex={1}
+					/>
+				</BarChart>
+			</ChartContainer>
+		</>
+	);
 }
